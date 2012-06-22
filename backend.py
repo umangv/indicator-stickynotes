f2371a8859cef5da0ea3cb20fbcab5cc7ecd0c85
Umang Varma <umang.me@gmail.com>
2012-06-21 20:06:16 -0400

    Licensed under GPLv3

--- backend.py
+++ backend.py
@@ -1,3 +1,20 @@
+# Copyright © 2012 Umang Varma <umang.me@gmail.com>
+# 
+# This file is part of indicator-stickynotes.
+# 
+# indicator-stickynotes is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+# 
+# indicator-stickynotes is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# indicator-stickynotes.  If not, see <http://www.gnu.org/licenses/>.
+
 from datetime import datetime
 import uuid
 import json
